f15c972ca0f553ce36ef1c8b78a35ffaa82b77f1
VictorienXP <victorienxp@orange.fr>
2015-02-12 10:41:33 +0100

    Welcome to GitHub ! ^^

---
+++ gamemodes/prophunters/gamemode/taunts/custom_xpsoundpack.lua
@@ -0,0 +1,111 @@
+addTaunt("Coucou tu veux voir ma bite ? [XPSP]", {
+	"prophunters/xpsoundpack/coucou_tu_veux_voir_ma_bite.wav"
+}, "both", "both", {"talk", "XPSoundPack"})
+addTaunt("What is love [XPSP]", {
+	"prophunters/xpsoundpack/what_is_love.mp3"
+}, "props", nil, {"music", "XPSoundPack"}, 15.5)
+addTaunt("Look at my horse [XPSP]", {
+	"prophunters/xpsoundpack/amazing_horse.mp3"
+}, "props", nil, {"music", "XPSoundPack"}, 25.5)
+addTaunt("Oh god, that's not a sword! [XPSP]", {
+	"prophunters/xpsoundpack/oh_god_thats_not_a_sword.mp3"
+}, "props", nil, {"music", "XPSoundPack"}, 13)
+addTaunt("Dancefloor Orgy [XPSP]", {
+	"prophunters/xpsoundpack/dancefloor_orgy.mp3"
+}, "props", nil, {"music", "XPSoundPack"}, 7.5)
+addTaunt("Cunegonde veux-tu du fromage? [XPSP]", {
+	"prophunters/xpsoundpack/cunegonde_veux-tu_du_fromage.mp3"
+}, "props", nil, {"music", "XPSoundPack"}, 13.5)
+addTaunt("Whee! [XPSP]", {
+	"prophunters/xpsoundpack/whee!.mp3"
+}, "props", nil, {"noise", "XPSoundPack"}, 14.9)
+addTaunt("FUS RO DAH [XPSP]", {
+	"prophunters/xpsoundpack/fusrodah.mp3"
+}, "hunters", nil, {"music", "XPSoundPack"}, 10.3)
+addTaunt("Shut up and take my money! [XPSP]", {
+	"prophunters/xpsoundpack/shut_up_and_take_my_money!.mp3"
+}, "props", "both", {"talk", "XPSoundPack"}, 1.3)
+addTaunt("HEYYEYAAEYAAAEYAEYAA [XPSP]", {
+	"prophunters/xpsoundpack/hey.mp3"
+}, "props", nil, {"music", "XPSoundPack"}, 12.5)
+addTaunt("Je veux faire l'amour a... [XPSP]", {
+	"prophunters/xpsoundpack/fromagedechevre.mp3"
+}, "props", nil, {"music", "XPSoundPack"}, 3.8)
+addTaunt("J'aime mettre mon penis dans... [XPSP]", {
+	"prophunters/xpsoundpack/penis&yaourt.mp3"
+}, "props", nil, {"music", "XPSoundPack"}, 14)
+addTaunt("J'aime me masturber dans les... [XPSP]", {
+	"prophunters/xpsoundpack/masturbationhautesherbes.mp3"
+}, "props", nil, {"music", "XPSoundPack"}, 3.5)
+addTaunt("Can't Killean the Zilean [XPSP]", {
+	"prophunters/xpsoundpack/can't_killean_the_zilean.mp3"
+}, "props", nil, {"music", "XPSoundPack"}, 7.2)
+addTaunt("Dream a Dream [XPSP]", {
+	"prophunters/xpsoundpack/dream.mp3"
+}, "props", nil, {"music", "XPSoundPack"}, 14.5)
+addTaunt("Enfant des jurons, avancement quete [XPSP]", {
+	"prophunters/xpsoundpack/enfantjuronquete.mp3"
+}, "props", "both", {"talk", "XPSoundPack"}, 4)
+addTaunt("Infogrames [XPSP]", {
+	"prophunters/xpsoundpack/sm.mp3"
+}, "props", "both", {"talk", "XPSoundPack"}, 4.3)
+addTaunt("Comment on fait les bebes ? [XPSP]", {
+	"prophunters/xpsoundpack/commentonfaitlesbebe.mp3"
+}, "props", "both", {"talk", "XPSoundPack"}, 5)
+addTaunt("Retourne toi que je m'identifie en toi [XPSP]", {
+	"prophunters/xpsoundpack/identifierentoi.mp3"
+}, "props", "both", {"talk", "XPSoundPack"}, 2.1)
+addTaunt("Caca chez Paul [XPSP]", {
+	"prophunters/xpsoundpack/cacachezpaul.mp3"
+}, "props", "both", {"talk", "XPSoundPack"}, 1.8)
+addTaunt("Hentai [XPSP]", {
+	"prophunters/xpsoundpack/hentai.mp3"
+}, "props", nil, {"music", "XPSoundPack"}, 4.3)
+addTaunt("Gayyyyyyyyyyyyyyy [XPSP]", {
+	"prophunters/xpsoundpack/gay.mp3"
+}, "props", "both", {"talk", "XPSoundPack"}, 2.1)
+addTaunt("Je regarde un porno ! [XPSP]", {
+	"prophunters/xpsoundpack/jeregardeunporno.mp3"
+}, "props", "both", {"talk", "XPSoundPack"}, 1.9)
+addTaunt("Nicolas Cage Wants Cake [XPSP]", {
+	"prophunters/xpsoundpack/nicholas_cage_wants_cake.mp3"
+}, "props", nil, {"music", "XPSoundPack"}, 6.9)
+addTaunt("Wingardium Leviosa (Ron) [XPSP]", {
+	"prophunters/xpsoundpack/wingardium_leviosa_ron.mp3"
+}, "props", "both", {"talk", "XPSoundPack"}, 7.9)
+addTaunt("Wingardium Leviosa (Hermione) [XPSP]", {
+	"prophunters/xpsoundpack/wingardium_leviosa_hermione.mp3"
+}, "props", "both", {"talk", "XPSoundPack"}, 2.3)
+addTaunt("Je code avec le cul [XPSP]", {
+	"prophunters/xpsoundpack/je_code_avec_le_cul.mp3"
+}, "props", nil, {"music", "XPSoundPack"}, 17.8)
+addTaunt("3615 Sado Mario [XPSP]", {
+	"prophunters/xpsoundpack/3615_sado_mario.mp3"
+}, "props", "both", {"talk", "XPSoundPack"}, 7.2)
+addTaunt("Une souris verte [XPSP]", {
+	"prophunters/xpsoundpack/une_souris_verte.mp3"
+}, "props", nil, {"music", "XPSoundPack"}, 23.5)
+addTaunt("Je vais te baiser... [XPSP]", {
+	"prophunters/xpsoundpack/je_vais_te_baiser.mp3"
+}, "props", nil, {"music", "XPSoundPack"}, 6.5)
+addTaunt("Faisez moi le bisou [XPSP]", {
+	"prophunters/xpsoundpack/faisez_moi_le_bisou.mp3"
+}, "props", "both", {"talk", "XPSoundPack"}, 4.3)
+addTaunt("Vas-y comme ça... [XPSP]", {
+	"prophunters/xpsoundpack/vas-y_comme_ca.mp3"
+}, "props", "both", {"talk", "XPSoundPack"}, 3.3)
+addTaunt("Je le savais bien que... [XPSP]", {
+	"prophunters/xpsoundpack/je_le_savais_bien_que.mp3"
+}, "props", "both", {"talk", "XPSoundPack"}, 1.6)
+addTaunt("C'est d'la merde ta magie ! [XPSP]", {
+	"prophunters/xpsoundpack/c'est_de_la_merde_ta_magie.mp3"
+}, "props", "both", {"talk", "XPSoundPack"}, 1.3)
+addTaunt("Je vous ai bien niqués ! [XPSP]", {
+	"prophunters/xpsoundpack/je_vous_ai_bien_niques.mp3"
+}, "props", "both", {"talk", "XPSoundPack"}, 2.2)
+addTaunt("Les Animaux du Monde [XPSP]", {
+	"prophunters/xpsoundpack/les_animaux_du_monde.mp3"
+}, "props", "both", {"talk", "XPSoundPack"}, 15.3)
+addTaunt("Oui je me balance ! [XPSP]", {
+	"prophunters/xpsoundpack/oui_je_me_balance.mp3"
+}, "props", nil, {"music", "XPSoundPack"}, 8.5)
---
+++ lua/autorun/server/prophuntersxperidiasoundpack.lua
@@ -0,0 +1,1 @@
+resource.AddWorkshop( "271944989" )
